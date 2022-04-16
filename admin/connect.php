df78f6c06084b29609b379517e6ade2a710cd65c
harshalvaidya10 <vaidyahome.hv@gmail.com>
2022-04-16 16:17:28 +0530

    make it better 3

--- admin/connect.php
+++ admin/connect.php
@@ -1,4 +1,10 @@
 <?php
 	$conn = new mysqli('remotemysql.com', 'vj7a50ab0v', 'ZnM7B4GZcW', 'vj7a50ab0v') or die(mysqli_error());
-
+	if ($conn -> connect_errno) {
+		echo "Failed to connect to MySQL: " . $conn -> connect_error;
+		exit();
+	  }
+	  else {
+		  echo "Connected";
+	  }
 ?>
