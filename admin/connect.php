0a68edaeb0efc8708c84f6f85dd229be4768eac8
harshalvaidya10 <vaidyahome.hv@gmail.com>
2022-04-16 16:11:20 +0530

    make it better 2

--- admin/connect.php
+++ admin/connect.php
@@ -1,2 +1,4 @@
 <?php
-	$conn = new mysqli('localhost', 'root', '', 'db_sars') or die(mysqli_error());
+	$conn = new mysqli('remotemysql.com', 'vj7a50ab0v', 'ZnM7B4GZcW', 'vj7a50ab0v') or die(mysqli_error());
+
+?>
